edbda72d60574c960430397a34f32e3044410bb6
chunsheng.li <chunsheng.li@99bill.com>
2021-06-24 15:38:41 +0800

    add

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   plugins: ['react-dev-inspector/plugins/umi/react-inspector'],
-  // https://github.com/zthxxx/react-dev-inspector#inspector-loader-props
   inspectorConfig: {
     exclude: [],
     babelPlugins: [],
